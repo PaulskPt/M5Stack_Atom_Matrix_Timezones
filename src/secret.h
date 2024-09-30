7ecf1a1755d195f566b21a9194543b2f75981536
Paulus H.J. Schulinck <PaulskPt@users.noreply.github.com>
2024-09-30 03:11:22 +0100

    Update secret.h
    deletion of comment

--- src/secret.h
+++ src/secret.h
@@ -1,5 +1,5 @@
 #define SECRET_SSID "<Your WiFi SSID here>"
 #define SECRET_PASS "<Your WiFi Password here>"
 #define SECRET_NTP_TIMEZONE "Europe/Lisbon"
-#define SECRET_NTP_TIMEZONE_CODE "WET0WEST,M3.5.0/1,M10.5.0" // Europe/Lisbon
+#define SECRET_NTP_TIMEZONE_CODE "WET0WEST,M3.5.0/1,M10.5.0"
 #define SECRET_NTP_SERVER_1 "2.pt.pool.ntp.org"
